3092d6a124a2f6c588e8de11715f638e1a8d5a28
kassing <kassing@live.cn>
2026-05-01 00:02:25 +0800

    Initial Commit

---
+++ DJI BLE Control Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DJI BLE Control Watch App
+//
+//  Created by 张有宽 on 1/5/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DJI BLE Control Watch App/DJI_BLE_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  DJI_BLE_ControlApp.swift
+//  DJI BLE Control Watch App
+//
+//  Created by 张有宽 on 1/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct DJI_BLE_Control_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
